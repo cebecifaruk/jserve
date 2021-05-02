7f3b4bd40af9d86b8479598f27de9bf64b1321cb
Faruk CEBECI <cebecifaruk@gmail.com>
2021-05-02 22:52:22 +0300

    createServer

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ import process from "process";
 import chokidar from "chokidar";
 import jserve from "./index.js";
 import path from "path";
-import fs from "fs";
+import fs, { rm } from "fs";
 import url from "url";
 import childProcess from "child_process";
 import ora from "ora";
@@ -15,6 +15,8 @@ const green = (...args) => console.log(...args.map((x) => chalk.green(x)));
 const red = (...args) => console.log(...args.map((x) => chalk.red(x)));
 const blue = (...args) => console.log(...args.map((x) => chalk.blue(x)));
 const gray = (...args) => console.log(...args.map((x) => chalk.gray(x)));
+
+const config = {};
 
 const spin = (text, action) =>
   new Promise((res, rej) => {
@@ -67,7 +69,7 @@ const exec = async (command, options) => {
 const params = yargs(process.argv.slice(2))
   .scriptName("jserve")
   .usage("$0 <cmd> [args]")
-  .command(["serve <script>", "$0"], "serve a javascript file", {
+  .command(["serve <script>", "$0"], "Serve a javascript file", {
     w: {
       alias: "http",
       demandOption: false,
@@ -110,6 +112,15 @@ const params = yargs(process.argv.slice(2))
       type: "boolean",
     },
   })
+  .command("start", "[TODO] Start jserve daemon", {})
+  .command("stop", "[TODO] Stop jserve daemon", {})
+  .command("add", "[TODO] Add a server to configuration file", {})
+  .command("status", "[TODO] Print the current state of the daemon", {})
+  .command("rm", "[TODO] Remove a server", {})
+  .command("log", "[TODO] Get the log of a process", {})
+  .command("reload", "[TODO] Reload config file", {})
+  .command("startup", "[TODO] Add jserve to the startup")
+  .option("conf", {})
   .epilog("Javascript API serving tool").argv;
 
 const commmands = {
@@ -117,7 +128,7 @@ const commmands = {
     const version = JSON.parse(
       await fs.promises.readFile(new URL("package.json", import.meta.url))
     ).version;
-    const templateString = await fs.promises.readFile(
+    const templateString = await fs.promisces.readFile(
       path.join(
         path.dirname(url.fileURLToPath(import.meta.url)),
         "templates",
@@ -161,48 +172,58 @@ const commmands = {
     blue("npm run serve");
   },
   async serve({ http, tcp, udp, script, watch }) {
-    blue("Server is starting...");
-
-    process.on("exit", () => {
-      if (!(http | tcp | udp))
-        red("Please check your script or your parameters");
-    });
-
-    process.on("SIG_TERM", () => red("Server is stopped"));
-
-    // ===== Loading Scripts =====
-
     const environment = {
       current: null,
     };
 
-    async function updateAPI(script) {
-      try {
-        environment.current = await import(path.resolve(script));
-        green("Script has been loaded.");
-      } catch (e) {
-        if (typeof err === "object") {
-          if (err.message) {
-            red("\nMessage: " + err.message);
-          }
-          if (err.stack) {
-            red("\nStacktrace:");
-            red("====================");
-            red(err.stack);
-          }
-        } else red("\n" + String(e));
-      }
-    }
+    // async function updateAPI(script) {
+    //   try {
+    //     environment.current = ;
+    //   } catch (e) {
+    //     throw e;
+    //     if (typeof err === "object") {
+    //       if (err.message) {
+    //         red("\nMessage: " + err.message);
+    //       }
+    //       if (err.stack) {
+    //         red("\nStacktrace:");
+    //         red("====================");
+    //         red(err.stack);
+    //       }
+    //     } else red("\n" + String(e));
+    //   }
+    // }
 
+    const updateAPI = async (script) =>
+      await spin(
+        "Loading script",
+        import(path.resolve(script)).then(
+          (result) => (environment.current = result)
+        )
+      );
     await updateAPI(script);
     if (watch)
-      chokidar
-        .watch(path.resolve(script))
-        .on("change", (_, _script) => updateAPI(path.resolve(script)));
+      chokidar.watch(path.resolve(script)).on("change", () =>
+        spin(
+          "Updating script",
+          fs.promises
+            .readFile(path.resolve(script))
+            .then((src) => import("data:text/javascript," + src))
+            .then((result) => (environment.current = result))
+        )
+      );
 
-    await jserve(environment.current, { http, udp, tcp });
-    green("Server has been started.");
+    const server = await jserve(environment.current, {
+      HTTP_PORT: http,
+      UDP_PORT: udp,
+      TCP_PORT: tcp,
+    });
   },
+  async add() {},
+  async status() {},
+  async rm() {},
+  async log() {},
+  async reload() {},
 };
 
 commmands[params._.length > 0 ? params._[0] : "serve"](params);
