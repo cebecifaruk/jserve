8d617dbf6a3a354c2afe190de044cba84af0eb13
Faruk CEBECI <cebecifaruk@gmail.com>
2021-05-09 02:43:20 +0300

    Updated cli

--- cli.js
+++ cli.js
@@ -6,18 +6,17 @@ import process from "process";
 import chokidar from "chokidar";
 import jserve from "./index.js";
 import path from "path";
-import fs, { rm } from "fs";
+import fs from "fs";
 import url from "url";
 import childProcess from "child_process";
 import ora from "ora";
 import untildify from "untildify";
+import fsExtra from "fs-extra";
 
 const green = (...args) => console.log(...args.map((x) => chalk.green(x)));
 const red = (...args) => console.log(...args.map((x) => chalk.red(x)));
 const blue = (...args) => console.log(...args.map((x) => chalk.blue(x)));
 const gray = (...args) => console.log(...args.map((x) => chalk.gray(x)));
-
-const config = {};
 
 const spin = (text, action) =>
   new Promise((res, rej) => {
@@ -67,136 +66,123 @@ const exec = async (command, options) => {
   });
 };
 
+function set(obj, key, value) {
+  const result = { ...obj };
+  result[key] = value;
+  return result;
+}
+
 const params = yargs(process.argv.slice(2))
   .scriptName("jserve")
   .usage("$0 <cmd> [args]")
   .command(["serve <script>", "$0"], "Serve a javascript file", {
     w: {
       alias: "http",
-      demandOption: false,
       default: 8080,
       describe: "Watch the script file",
       type: "number",
     },
     u: {
       alias: "udp",
-      demandOption: false,
       default: 8090,
       describe: "Raw UDP Server Port",
       type: "number",
     },
     t: {
       alias: "tcp",
-      demandOption: false,
       default: 8090,
       describe: "Raw TCP Server Port",
       type: "number",
     },
     watch: {
-      demandOption: false,
       default: false,
       describe: "watch mode",
       type: "boolean",
     },
   })
-  .command("init <project>", "create a project from a template", {
-    template: {
-      demandOption: false,
-      default: "simple",
-      describe: "Template",
-      type: "string",
-    },
-    "list-templates": {
+  .command("start <conf>", "Start jserve with a configuration file", {
+    dns: {
       default: false,
-      demandOption: false,
-      describe: "List available project templates",
+      describe: "DNS Server",
+      type: "boolean",
+    },
+    tls: {
+      default: false,
+      describe: "TLS Support",
       type: "boolean",
     },
   })
-  .command("start", "[TODO] Start jserve daemon", {})
-  .command("stop", "[TODO] Stop jserve daemon", {})
-  .command("add", "[TODO] Add a server to configuration file", {})
-  .command("status", "[TODO] Print the current state of the daemon", {})
-  .command("rm", "[TODO] Remove a server", {})
-  .command("log", "[TODO] Get the log of a process", {})
-  .command("reload", "[TODO] Reload config file", {})
-  .command("startup", "[TODO] Add jserve to the startup")
-  .option("conf", {
-    default: "~/.jserve.config.json",
-    describe: "Path of the configuration file",
-    type: "string",
+  .command("init <project> [template]", "Create a project from a template.", {
+    path: {
+      default: "./",
+      describe: "Path of the project will be created.",
+      type: "string",
+    },
   })
+  .command("test <template>", "Test a template", {})
+  .command("list-templates", "List available project templates.")
+  .command("startup", "Add jserve to the startup", {
+    initSystem: {
+      default: "",
+      describe: "Name of the init system.",
+      type: "string",
+    },
+    write: {
+      default: false,
+      describe: "Write startup script to the disk.",
+      type: "boolean",
+    },
+  })
+  .command("status", "Print the current state of the daemon", {})
+  .command("log", "Get the log of a process", {})
+  .command("conf-init [conf]", "Create a configuration file")
+  .command("conf-show [conf]", "Show a config file")
+  .command(
+    "conf-set <key> <value> [conf]",
+    "Set a property to a value in a conf file"
+  )
+  .command("conf-add <name> <script> [conf]", "Add a server to a conf file")
+  .command("conf-rm <name> [conf]", "Remove a server from a conf file")
+  .alias("v", "version")
+  .alias("h", "help")
+  .example("deneme")
   .epilog("Javascript API serving tool").argv;
 
-const conf = {
-  current: {},
-  path: params.conf,
-  async read() {
-    const exists = (f) =>
-      fs.promises
-        .access(f)
-        .then(() => true)
-        .catch(() => false);
-    const confPath = untildify(conf.path);
-    if (!(await exists(confPath))) await fs.promises.writeFile(confPath, "{}");
-    conf.current = JSON.parse(await fs.promises.readFile(confPath));
-    return conf;
-  },
-  async write() {
-    await fs.promises.writeFile(
-      untildify(conf.path),
-      JSON.stringify(conf.current, null, 4)
-    );
-  },
-};
-
-conf
-  .read()
-  .then(console.log)
-  .then(() => {
-    conf.current = { a: 1 };
-    conf.write();
-  });
+// const conf = {
+//   current: {},
+//   path: params.conf,
+//   async read() {
+//     const exists = (f) =>
+//       fs.promises
+//         .access(f)
+//         .then(() => true)
+//         .catch(() => false);
+//     const confPath = untildify(conf.path);
+//     if (!(await exists(confPath))) await fs.promises.writeFile(confPath, "{}");
+//     conf.current = JSON.parse(await fs.promises.readFile(confPath));
+//     return conf;
+//   },
+//   async write() {
+//     await fs.promises.writeFile(
+//       untildify(conf.path),
+//       JSON.stringify(conf.current, null, 4)
+//     );
+//   },
+// };
 
 const commmands = {
-  async init({ project, template }) {
-    const version = JSON.parse(
-      await fs.promises.readFile(new URL("package.json", import.meta.url))
-    ).version;
-    const templateString = await fs.promisces.readFile(
-      path.join(
-        path.dirname(url.fileURLToPath(import.meta.url)),
-        "templates",
-        template + ".js"
-      )
-    );
+  async init({ project, template = "simple" }) {
     await spin("Creating project directory", fs.promises.mkdir(project));
     await spin(
-      "Creating package.json",
-      fs.promises.writeFile(
-        path.join(project, "package.json"),
-        JSON.stringify(
-          {
-            name: project,
-            version: "1.0.0",
-            description: "",
-            main: "main.js",
-            type: "module",
-            scripts: {
-              serve: "jserve main.js",
-            },
-            dependencies: {
-              "@cebecifaruk/jserve": "^" + version,
-            },
-          },
-          null,
-          4
-        )
+      "Copying template",
+      fsExtra.copy(
+        path.join(
+          path.dirname(url.fileURLToPath(import.meta.url)),
+          "templates",
+          template
+        ),
+        path.resolve(untildify(project))
       )
-    );
-    spin(
-      "Creating main.js",
-      fs.promises.writeFile(path.join(project, "main.js"), templateString)
     );
     await spin(
       "Installing npm dependencies",
@@ -254,11 +240,94 @@ const commmands = {
       TCP_PORT: tcp,
     });
   },
-  async add() {},
-  async status() {},
-  async rm() {},
-  async log() {},
-  async reload() {},
+  async status() {
+    console.log("TODO");
+  },
+  "conf-init": async ({ conf = "jserve.config.json" }) => {
+    await spin(
+      "Creating configuration file",
+      fs.promises.writeFile(
+        conf,
+        JSON.stringify(
+          {
+            httpPort: 80,
+            httpsPort: 443,
+            udpPort: 3000,
+            tcpPort: 3000,
+            watch: true,
+            domains: [],
+          },
+          null,
+          4
+        )
+      )
+    );
+  },
+  "conf-rm": async ({ conf = "jserve.config.json", key, value }) => {},
+  "conf-set": async ({ conf = "jserve.config.json", key, value }) => {
+    const confContent = await spin(
+      "Reading configuration file",
+      fs.promises.readFile(conf).then(JSON.parse)
+    );
+    try {
+      value = JSON.parse(value);
+    } catch (e) {}
+    await spin(
+      "Updating configuration file",
+      fs.promises.writeFile(
+        conf,
+        JSON.stringify(set(confContent, key, value), null, 4)
+      )
+    );
+  },
+  "conf-show": async ({ conf = "jserve.config.json" }) => {
+    console.log(String(await fs.promises.readFile(conf)));
+  },
+  async log() {
+    console.log("TODO");
+  },
+  async test() {
+    fs.mkdtemp(path.join(os.tmpdir(), "foo-"), (err, folder) => {
+      if (err) throw err;
+      console.log(folder);
+      // Prints: /tmp/foo-itXde2
+    });
+  },
+  async startup({ initSystem, write }) {
+    initSystem =
+      initSystem === ""
+        ? await spin("Detecting init system.", async () => {
+            const platform = process.platform;
+            return null;
+          })
+        : initSystem;
+
+    switch (initSystem) {
+      case "windows":
+      // Windows
+      case "launchd":
+      // Mac
+      case "systemv":
+      case "systemd":
+      case "openrc":
+      case "runit":
+      case "initrc":
+      // BSD
+      default:
+        red("Unsupported init system.");
+        gray("You can add jserve to your init system manually");
+        gray("Just run this command when system has been boot up:");
+        gray("jserve");
+        return;
+    }
+
+    // childProcess.fork(
+    //   path.join(path.dirname(url.fileURLToPath(import.meta.url)), "cli.js"),
+    //   {
+    //     cwd: "/",
+    //   }
+    // );
+  },
 };
 
 commmands[params._.length > 0 ? params._[0] : "serve"](params);
--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-import {
+export {
   createServer,
   createHttpServer,
   createUdpServer,
   createTcpServer,
 } from "./src/createServer.js";
-import { Html, Static } from "./src/Types.js";
+export { Html, Static } from "./src/Types.js";
 
-export {
-  Html,
-  Static,
-  createServer,
-  createHttpServer,
-  createUdpServer,
-  createTcpServer,
-};
-
+import createServer from "./src/createServer.js";
 export default createServer;
